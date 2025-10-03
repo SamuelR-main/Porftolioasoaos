722026e4f8f996e111ebc439eba6804621f0f421
Samuel <saaamuelrooodrigooo@gmail.com>
2025-10-03 17:04:33 -0300

    conquistas

---
+++ GetConquistas/popup.js
@@ -0,0 +1,6 @@
+document.getElementById("botao").addEventListener("click", () => {
+        alert("Working");
+        document.querySelector("body").style.backgroundColor = "#000000";
+    }
+);
+    
